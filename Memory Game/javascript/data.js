eae0646bc0440bec30a9d363debf39f1776fb0b1
Janko <45310863+WatchJani@users.noreply.github.com>
2023-04-28 22:49:55 +0200

    Add files via upload

---
+++ Memory Game/javascript/data.js
@@ -0,0 +1,102 @@
+let data = [
+    {
+        "id": 1,
+        "img": "../../assets/1.webp",
+        "key":1
+    },
+    {
+        "id": 2,
+        "img": "../../assets/2.webp",
+        "key":2
+    },
+    {
+        "id": 3,
+        "img": "../../assets/3.webp",
+        "key":3
+    },
+    {
+        "id": 4,
+        "img": "../../assets/4.webp",
+        "key":4
+    },
+    {
+        "id": 5,
+        "img": "../../assets/5.webp",
+        "key":5
+    },
+    {
+        "id": 6,
+        "img": "../../assets/6.webp",
+        "key":6
+    },
+    {
+        "id": 7,
+        "img": "../../assets/7.webp",
+        "key":7
+    },
+    {
+        "id": 8,
+        "img": "../../assets/8.webp",
+        "key":8
+    },
+    {
+        "id": 9,
+        "img": "../../assets/9.webp",
+        "key":9
+    },
+    {
+        "id": 10,
+        "img": "../../assets/10.webp",
+        "key":10
+    },
+    {
+        "id": 11,
+        "img": "../../assets/1.webp",
+        "key":1
+    },
+    {
+        "id": 12,
+        "img": "../../assets/2.webp",
+        "key":2
+    },
+    {
+        "id": 13,
+        "img": "../../assets/3.webp",
+        "key":3
+    },
+    {
+        "id": 14,
+        "img": "../../assets/4.webp",
+        "key":4
+    },
+    {
+        "id": 15,
+        "img": "../../assets/5.webp",
+        "key":5
+    },
+    {
+        "id": 16,
+        "img": "../../assets/6.webp",
+        "key":6
+    },
+    {
+        "id": 17,
+        "img": "../../assets/7.webp",
+        "key":7
+    },
+    {
+        "id": 18,
+        "img": "../../assets/8.webp",
+        "key":8
+    },
+    {
+        "id": 19,
+        "img": "../../assets/9.webp",
+        "key":9
+    },
+    {
+        "id": 20,
+        "img": "../../assets/10.webp",
+        "key":10
+    }
+]
---
+++ Memory Game/javascript/index.js
@@ -0,0 +1,63 @@
+//random array
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+data = shuffleArray(data);
+
+//get element from html
+const slikeDiv = document.getElementById("img");
+
+//object array 
+const htmlString = data.map(obj => {
+    return `<img src="../../assets/background-card.webp" alt="Slika ${obj.id}" id="${obj.id}" onClick="Read(${obj.id}, ${obj.key})">`;
+}).join("");
+
+//print data from array
+slikeDiv.innerHTML = htmlString;
+
+let card = {
+    "thisClick": null,
+    "previousCard": null
+}
+
+let isAnimationRunning = false
+
+const Read = (cardId, cardKey) => {
+    if (isAnimationRunning) {
+        return;
+    }
+
+    let thisImage = document.getElementById(cardId);
+
+    thisImage.classList.add('animate');
+
+
+    let previousImage = document.getElementById(card.previousCard)
+    thisImage.src = `../../assets/${cardKey}.webp`
+
+    if (card.previousCard == null) {
+        card.previousCard = cardId
+        return
+    }
+
+    if (card.previousCard % 10 == cardId % 10) { // the best 😎😎😎
+        console.log("win")
+        card.previousCard = null
+        return
+    }
+    isAnimationRunning = true
+    card.previousCard = null
+
+    setTimeout(function () {
+        thisImage.classList.remove('animate');
+        previousImage.classList.remove('animate');
+        thisImage.src = `../../assets/background-card.webp`
+        previousImage.src = `../../assets/background-card.webp`
+        isAnimationRunning = false
+    }, 1000);
+}
